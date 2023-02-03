0ad33a1dd61ba99e77e3148bb9b222784b9aa607
kz-rmz <magazumov@gmail.com>
2023-02-03 11:37:56 +0600

    /api/newmessages unnecessary imports deleted

--- pages/api/newmessage.ts
+++ pages/api/newmessage.ts
@@ -1,7 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import IMessageObject from "@/lib/IMessageObject";
-import path from "node:path/win32";
-import fs from "fs";
 import newMessage from "@/lib/getMessages";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
